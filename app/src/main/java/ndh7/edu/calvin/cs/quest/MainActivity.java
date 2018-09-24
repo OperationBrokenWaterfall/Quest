a1d2e5f914dec6fcd9ef499bba403d3c502e203c
nherder41 <nherder41@users.noreply.github.com>
2018-09-24 13:52:13 -0400

    lab03 test prototype application

---
+++ app/src/main/java/ndh7/edu/calvin/cs/quest/MainActivity.java
@@ -0,0 +1,13 @@
+package ndh7.edu.calvin.cs.quest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
