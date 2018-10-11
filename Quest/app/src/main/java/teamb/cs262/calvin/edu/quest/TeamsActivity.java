a8d49571883b3b2d66bd07165e5af5df09e6d0ca
nherder41 <nateherder@gmail.com>
2018-10-11 15:59:50 -0400

    prototype fixes to open the correct fragment by default

--- Quest/app/src/main/java/teamb/cs262/calvin/edu/quest/TeamsActivity.java
+++ Quest/app/src/main/java/teamb/cs262/calvin/edu/quest/TeamsActivity.java
@@ -17,6 +17,25 @@ public class TeamsActivity extends AppCompatActivity {
 
     private Fragment fragment;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teams);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //set the action bar to the team name
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String team = bundle.getString("team_name");
+        getSupportActionBar().setTitle("Team: " + team);
+
+        //open into the TaskListFragment by default
+        if (savedInstanceState == null) {
+            navigation.setSelectedItemId(R.id.task_list_fragment); // change to whichever id should be default
+        }
+    }
+
     //listen for a click on the navigation
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -27,42 +46,27 @@ public class TeamsActivity extends AppCompatActivity {
             //Start the fragment that is clicked
             switch (item.getItemId()) {
                 case R.id.qr_scanner_fragment:
-                    return true;
+                    break;
                 case R.id.leaderboard_fragment:
-                    startFragment(LeaderBoardFragment.newInstance());
-                    return true;
+                    fragment = new LeaderBoardFragment();
+                    break;
                 case R.id.task_list_fragment:
-                    startFragment(TaskListFragment.newInstance());
-                    return true;
+                    fragment = new TaskListFragment();
+                    break;
             }
-            return false;
+            return startFragment(fragment);
         }
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teams);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        //open into the TaskListFragment by default
-        startFragment(TaskListFragment.newInstance());
-
-        //set the action bar to the team name
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-        String team = bundle.getString("team_name");
-        getSupportActionBar().setTitle("Team: " + team);
-    }
-
     //this method opens a fragment if it is not opened
-    private void startFragment(Fragment fragment) {
+    private boolean startFragment(Fragment fragment) {
         if(fragment != null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             transaction.replace(R.id.container, fragment);
             transaction.commit();
+            return true;
         }
+        return false;
     }
 
     //disable the android back button
